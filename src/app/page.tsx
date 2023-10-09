2b19ae2b6448f50ec953ae78460e8568bd6697f3
Mihir <85486112+DYNAMICMORTAL@users.noreply.github.com>
2023-10-09 10:00:20 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,16 +10,16 @@ export default function Home() {
       <MaxWidthWrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'>
         <div className='mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50'>
           <p className='text-sm font-semibold text-gray-700'>
-            Quill is now public!
+            S.T.A.R. is now public!
           </p>
         </div>
         <h1 className='max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl'>
-          Chat with your{' '}
+          Well maintain your{' '}
           <span className='text-blue-600'>documents</span>{' '}
           in seconds.
         </h1>
         <p className='mt-5 max-w-prose text-zinc-700 sm:text-lg'>
-          Quill allows you to have conversations with any
+          S.T.A.R. allows you to have conversations with any
           PDF document. Simply upload your file and start
           asking questions right away.
         </p>
@@ -91,7 +91,7 @@ export default function Home() {
             </h2>
             <p className='mt-4 text-lg text-gray-600'>
               Chatting to your PDF files has never been
-              easier than with Quill.
+              easier than with S.T.A.R.
             </p>
           </div>
         </div>
@@ -107,14 +107,7 @@ export default function Home() {
                 Sign up for an account
               </span>
               <span className='mt-2 text-zinc-700'>
-                Either starting out with a free plan or
-                choose our{' '}
-                <Link
-                  href='/pricing'
-                  className='text-blue-700 underline underline-offset-2'>
-                  pro plan
-                </Link>
-                .
+                Sign Up and use it for free.
               </span>
             </div>
           </li>
@@ -141,7 +134,7 @@ export default function Home() {
                 Start asking questions
               </span>
               <span className='mt-2 text-zinc-700'>
-                It&apos;s that simple. Try out Quill today -
+                It&apos;s that simple. Try out S.T.A.R. today -
                 it really takes less than a minute.
               </span>
             </div>
